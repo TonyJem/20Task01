fd0422d3eef5efb0e582209c22978c4e83ae2f59
TonyJem <a.jemanov@gmail.com>
2021-01-10 12:25:49 +0200

    Add UIViewCollection to ViewController

--- ExploringCollectionView/ExploringCollectionView/ViewController.swift
+++ ExploringCollectionView/ExploringCollectionView/ViewController.swift
@@ -1,8 +1,22 @@
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet private weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as UICollectionViewCell
+        return cell
     }
 }
